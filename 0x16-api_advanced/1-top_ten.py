6b78954c7c22b1316e1df8a806ec9641afad8101
Moealsir <mohamedwdalsir@gmail.com>
2024-05-07 19:37:20 +0300

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
-""" Function that queries the Reddit API """
+"""
+return top
+"""
 import requests
-import sys
 
 
 def top_ten(subreddit):
-    """Returns: top ten post titles
-    or None if queried subreddit is invalid"""
-    headers = {"User-Agent": "xica369"}
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    parameters = {"limit": 10}
-    try:
-        response = requests.get(url, headers=headers, params=parameters)
-        response.raise_for_status()
-        titles_ = response.json().get("data", {}).get("children", [])
-        for title_ in titles_:
-            print(title_.get("data", {}).get("title"))
-    except requests.RequestException as e:
-        print(None)
+    """Returns the number of subscribers for a given subreddit"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if not subreddit or type(subreddit) is not str:
+        return None
+    if response.status_code == 200:
+        data = response.json()
+        posts = data.get("data", {}).get("children", [])
+        if posts:
+            for post in posts:
+                print(post.get("data", {}).get("title"))
+    else:
+        print('None')
