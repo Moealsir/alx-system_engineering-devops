332bf5a48b6214554a04fa80c1ba62125883deeb
Moealsir <mohamedwdalsir@gmail.com>
2024-05-07 19:54:21 +0300

    2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -9,18 +9,18 @@ def recurse(subreddit, hot_list=[], after=None):
     """Returns the number of subscribers for a given subreddit"""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-    params = {"limit": 100, 'after': after} if after else {"limit": 100}
+    params = {"limit": 100, "after": after} if after else {"limit": 100}
 
-    response = requests.get(url, headers=headers,
-                  params=params, allow_redirects=False)
+    response = requests.get(
+        url, headers=headers, params=params, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
-        posts = data.get('data', {}).get('children', [])
+        posts = data.get("data", {}).get("children", [])
         for post in posts:
-            hot_list.append(post['data']['title'])
+            hot_list.append(post["data"]["title"])
 
-        after = data['data'].get('after')
+        after = data["data"].get("after")
 
         if after:
             return recurse(subreddit, hot_list, after)
