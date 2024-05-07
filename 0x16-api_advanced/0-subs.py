c661eecad4329c1a9a1ad9d9044fef9232b3515d
Moealsir <mohamedwdalsir@gmail.com>
2024-05-07 18:36:15 +0300

    0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import requests
+
+
+def number_of_subscribers(subreddit):
+	"""Returns the number of subscribers for a given subreddit"""
+
+	url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+	headers = {
+		'User-Agent': 'Mozilla/5.0'
+	}
+	response = requests.get(url, headers=headers)
+
+	if response.status_code == 200:
+		return response.json().get('data').get('subscribers')
+	else:
+		return 0
